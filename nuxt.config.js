5aa2f9058c8faf36fc7f72bb6a39c2028ea34191
Mehmet <38668796+productfrontenddeveloper@users.noreply.github.com>
2021-04-07 17:20:01 +0300

    fix: general (#5)

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,8 +1,24 @@
-import { withDocus } from 'docus'
+import { withDocus } from "docus";
 
 // Learn more at https://docus.dev
 export default withDocus({
   docus: {
     // primaryColor: '#E24F55'
-  }
-})
+  },
+  i18n: {
+    locales: () => [
+      {
+        code: "en",
+        iso: "en-US",
+        file: "en-US.js",
+        name: "English",
+      },
+      {
+        code: "tr",
+        iso: "tr-TR",
+        file: "tr-TR.js",
+        name: "Türkçe",
+      },
+    ],
+  },
+});
